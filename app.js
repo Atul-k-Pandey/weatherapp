8e1765e10235ade643831a6d35d550ad4c3b8415
Atul kumar pandey <83165526+Atul-k-Pandey@users.noreply.github.com>
2024-08-30 10:11:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const weather = document.querySelector("#weather")
 const getWeather = async(city) => {
     weather.innerHTML = `<h2> Loading... <h2>`
     //const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}`
+    const url = `hhttps://weather.visualcrossing.com/VisualCrossingWebServices/rest/services
+          /timeline/London%2CUK/2022-07-15/2022-07-20?unitGroup=metric&contentType=csv&include=days&key=YOUR_API_KEY`
     const response = await fetch(url);
     const data = await response.json()
     return showWeather(data)
